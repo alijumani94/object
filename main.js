ac584c869c3671a7e1320ee021132e38d2d528c6
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-29 14:30:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var student = {
+    name: "Ali",
+    age: 28,
+    course: "Gen AI",
+    batch: "first batch"
+};
+console.log("My name is ".concat(student.name, ", I am ").concat(student.age, " years old. I am currently studying in ").concat(student.batch, " of ").concat(student.course, "."));
---
+++ main.ts
@@ -0,0 +1,8 @@
+let student= {
+    name: "Ali",
+    age: 28,
+    course: "Gen AI",
+    batch: "first batch"
+
+}
+console.log(`My name is ${student.name}, I am ${student.age} years old. I am currently studying in ${student.batch} of ${student.course}.`);
