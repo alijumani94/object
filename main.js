1bdcc6bd4a29dfab242abf3812e0d6d196dd34fe
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-29 15:20:32 +0500

    Add files via upload
    Made changes in which printed the object value with different method.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var student = {
     name: "Ali",
     age: 28,
-    course: "Gen AI",
+    course: "Gen-AI",
     batch: "first batch"
 };
-console.log("My name is ".concat(student.name, ", I am ").concat(student.age, " years old. I am currently studying in ").concat(student.batch, " of ").concat(student.course, "."));
+console.log("My name is ".concat(student["name"], ", I am ").concat(student["age"], " years old. I am currently studying in ").concat(student.batch, " of ").concat(student.course, "."));
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 let student= {
     name: "Ali",
     age: 28,
-    course: "Gen AI",
+    course: "Gen-AI",
     batch: "first batch"
 
 }
-console.log(`My name is ${student.name}, I am ${student.age} years old. I am currently studying in ${student.batch} of ${student.course}.`);
+console.log(`My name is ${student["name"]}, I am ${student["age"]} years old. I am currently studying in ${student.batch} of ${student.course}.`);
